6571945d54a1d946c3623c4708260741a7e9accb
AYON <sarkerbishwendu90@gmail.com>
2025-04-02 00:25:17 +0600

    slider adjustment

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,17 @@
 var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 });
