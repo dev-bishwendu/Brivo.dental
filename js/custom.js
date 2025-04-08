83e1f348307117254432fe0191a2f7ee14cdef6e
AYON <sarkerbishwendu90@gmail.com>
2025-04-08 20:33:22 +0600

    height adjst

--- js/custom.js
+++ js/custom.js
@@ -20,15 +20,11 @@ var swiper = new Swiper(".mySwiperTwo", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
   mousewheel: true,
     keyboard: true,
     loop: true,
     autoplay: {
-        delay: 1000,
+        delay: 1500,
         disableOnInteraction: false,
     },
 });
